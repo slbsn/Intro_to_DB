07fba5a3782daec63f6bf10cb44c8a97903f19b3
Ndumiso Tyam <138490666+slbsn@users.noreply.github.com>
2024-12-13 00:37:44 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,16 +1,8 @@
--- Create the database
 CREATE DATABASE alx_book_store;
-
--- Use the database
-USE alx_book_store;
-
--- Create the Authors table
 CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
-
--- Create the Books table
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
@@ -19,24 +11,18 @@ CREATE TABLE Books (
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-
--- Create the Customers table
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
-
--- Create the Orders table
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
--- Create the Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
