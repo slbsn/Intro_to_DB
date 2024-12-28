e5906f08829d973f5fb7f2361f34651367998ac9
Ndumiso Tyam <138490666+slbsn@users.noreply.github.com>
2024-12-28 18:04:38 +0200

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,8 @@
 -- task_3.sql: Script to list all tables in a given MySQL database
 
+-- Select the alx_book_store database
+USE alx_book_store;
+
 -- Display all tables in the specified database
 SHOW TABLES;
+
