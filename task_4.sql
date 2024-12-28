c4580680da85d3203fb6d89a41313796f910dd79
Ndumiso Tyam <138490666+slbsn@users.noreply.github.com>
2024-12-28 18:07:19 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- task_4.sql: Script to show full description of Books table
+
+-- Select the database
+USE alx_book_store;
+
+-- Show the full create table statement for Books table
+SHOW CREATE TABLE Books;
